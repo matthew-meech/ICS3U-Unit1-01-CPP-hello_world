7be0dd2ba554371ab3c3ad3aa6ac99661114ab88
Ubuntu <ubuntu@ip-172-31-24-85.ec2.internal>
2021-09-12 23:43:22 +0000

    hello world progam

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Matthew Meech All rights reserved
+//
+// Created by: Matthew Meech
+// Created on: Sept 2021
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+  
+int main() {
+  std::cout << "Hello, World!";
+}
